ca1eade397f16cdbc0593bed07072383a12be266
djserega <galyuk.sergey@gmail.com>
2018-11-18 02:04:28 +0200

    Added.UserControl.TimeControl

---
+++ src/LogBookReader/TimeControl.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LogBookReader
+{
+    /// <summary>
+    /// Логика взаимодействия для TimeControl.xaml
+    /// </summary>
+    public partial class TimeControl : UserControl
+    {
+        public TimeControl()
+        {
+            InitializeComponent();
+        }
+        public TimeSpan Value
+        {
+            get { return (TimeSpan)GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        }
+        public static readonly DependencyProperty ValueProperty =
+            DependencyProperty.Register(
+                "Value",
+                typeof(TimeSpan),
+                typeof(TimeControl),
+                new UIPropertyMetadata(DateTime.Now.TimeOfDay, new PropertyChangedCallback(OnValueChanged)));
+
+        private static void OnValueChanged(DependencyObject obj, DependencyPropertyChangedEventArgs e)
+        {
+            TimeControl control = obj as TimeControl;
+            control.Hours = ((TimeSpan)e.NewValue).Hours;
+            control.Minutes = ((TimeSpan)e.NewValue).Minutes;
+            control.Seconds = ((TimeSpan)e.NewValue).Seconds;
+        }
+
+        public int Hours
+        {
+            get { return (int)GetValue(HoursProperty); }
+            set { SetValue(HoursProperty, value); }
+        }
+        public static readonly DependencyProperty HoursProperty =
+            DependencyProperty.Register("Hours", typeof(int), typeof(TimeControl), new UIPropertyMetadata(0, new PropertyChangedCallback(OnTimeChanged)));
+
+        public int Minutes
+        {
+            get { return (int)GetValue(MinutesProperty); }
+            set { SetValue(MinutesProperty, value); }
+        }
+        public static readonly DependencyProperty MinutesProperty =
+            DependencyProperty.Register("Minutes", typeof(int), typeof(TimeControl), new UIPropertyMetadata(0, new PropertyChangedCallback(OnTimeChanged)));
+
+        public int Seconds
+        {
+            get { return (int)GetValue(SecondsProperty); }
+            set { SetValue(SecondsProperty, value); }
+        }
+
+        public static readonly DependencyProperty SecondsProperty =
+            DependencyProperty.Register("Seconds", typeof(int), typeof(TimeControl), new UIPropertyMetadata(0, new PropertyChangedCallback(OnTimeChanged)));
+
+
+        private static void OnTimeChanged(DependencyObject obj, DependencyPropertyChangedEventArgs e)
+        {
+            TimeControl control = obj as TimeControl;
+            control.Value = new TimeSpan(control.Hours, control.Minutes, control.Seconds);
+        }
+    }
+}
