fc7a0f7a6c77054b4adb5c861eff87bd7f9fb322
djserega <galyuk.sergey@gmail.com>
2018-11-19 09:44:21 +0200

    added: initializer connection file

---
+++ src/LogBookReader/EF/Initialize.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogBookReader.EF
+{
+    internal class Initialize
+    {
+        private readonly string _baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+        internal bool FindCreateConnectionFile()
+        {
+            string pathConfig = Path.Combine(_baseDirectory, "dbConnection.config");
+            FileInfo configFile = new FileInfo(pathConfig);
+            if (!configFile.Exists)
+            {
+                try
+                {
+                    using (StreamWriter stream = configFile.CreateText())
+                    {
+                        stream.WriteLine("<connectionStrings>");
+                        stream.WriteLine("  <add name=\"DefaultConnection\"");
+                        stream.WriteLine("       connectionString=\"Data Source=1Cv8.lgd\"");
+                        stream.WriteLine("       providerName=\"System.Data.SQLite\"/>");
+                        stream.WriteLine("</connectionStrings>");
+                        stream.Flush();
+                    }
+                    return true;
+
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
+            }
+            else
+                return true;
+        }
+    }
+}
